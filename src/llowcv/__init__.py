6dbf982c48de7f752ee1df6a7f8ee5fd5b8f28e7
Moge800 <benom0210@gmail.com>
2026-04-02 17:01:52 +0900

    __init__.pyを新規作成し、llowcvモジュールの基本機能を定義

---
+++ src/llowcv/__init__.py
@@ -0,0 +1,58 @@
+from llowcv.camera.base import Camera
+from llowcv.image import (
+    alpha_composite,
+    bgr2rgb,
+    blur,
+    blend,
+    composite,
+    crop,
+    flip,
+    imread,
+    imshow,
+    imwrite,
+    merge,
+    put_text,
+    resize,
+    rgb2bgr,
+    rotate,
+    sharpen,
+    split,
+    to_gray,
+)
+from llowcv.util import as_cv2, as_numpy, from_cv2, to_bgr
+
+__version__ = "0.1.0"
+
+__all__ = [
+    # camera
+    "Camera",
+    # image I/O
+    "imread",
+    "imwrite",
+    "imshow",
+    # transform
+    "resize",
+    "crop",
+    "rotate",
+    "flip",
+    # filter
+    "blur",
+    "sharpen",
+    # color
+    "to_gray",
+    "bgr2rgb",
+    "rgb2bgr",
+    "split",
+    "merge",
+    # draw
+    "put_text",
+    # composite
+    "blend",
+    "composite",
+    "alpha_composite",
+    # convert (opt-in)
+    "as_cv2",
+    "from_cv2",
+    "as_numpy",
+    "to_bgr",
+]
